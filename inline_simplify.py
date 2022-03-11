9f2d03039fdea9bd81812e361a5ac9cfd82d18c9
molpopgen <krthornt@uci.edu>
2022-03-11 13:10:29 -0800

    test pass again

--- inline_simplify.py
+++ inline_simplify.py
@@ -249,27 +249,30 @@ class Individual(object):
                 seg = Segment(left, right, mapped_ind)
                 # print(f"Adding ancestry {seg} to {self.ancestry} of {self.index}")
                 new_segment = True
-                # for i in self.ancestry:
-                #    if i.right > seg.left and seg.right > i.left:
-                #        new_segment = False
-                # if new_segment:
-                #    self.ancestry.append(seg)
-                while ancestry_index < ancestry_len:
-                    if self.ancestry[ancestry_index].left >= seg.left:
-                        break
-                    else:
-                        ancestry_index += 1
-
-                print(ancestry_index, ancestry_len, seg, self.ancestry)
-
-                if ancestry_index < ancestry_len:
-                    i = self.ancestry[ancestry_index]
+                for i in self.ancestry:
                     if i.right > seg.left and seg.right > i.left:
-                        new_segment = False
                         i.left = max(i.left, seg.left)
                         i.right = min(i.right, seg.right)
+                        i.child = mapped_ind
+                        new_segment = False
                 if new_segment:
                     self.ancestry.append(seg)
+                # while ancestry_index < ancestry_len:
+                #    if self.ancestry[ancestry_index].left >= seg.left:
+                #        break
+                #    else:
+                #        ancestry_index += 1
+
+                # print(ancestry_index, ancestry_len, seg, self.ancestry)
+
+                # if ancestry_index < ancestry_len:
+                #    i = self.ancestry[ancestry_index]
+                #    if i.right > seg.left and seg.right > i.left:
+                #        new_segment = False
+                #        i.left = max(i.left, seg.left)
+                #        i.right = min(i.right, seg.right)
+                # if new_segment:
+                #    self.ancestry.append(seg)
 
             print("final ancestry =", self.ancestry)
 
